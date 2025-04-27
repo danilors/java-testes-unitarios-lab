6420861fdd56900b64d95e4cfdd0094dead3cc20
Danilo Silva <danilors.work@gmail.com>
2025-04-27 14:29:58 -0300

    add module: api-todotarefas

---
+++ api-todotarefas/src/main/java/br/com/todotarefas/todotarefas/ApiTodotarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.todotarefas.todotarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTodotarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTodotarefasApplication.class, args);
+	}
+
+}
---
+++ api-todotarefas/src/test/java/br/com/todotarefas/todotarefas/ApiTodotarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.todotarefas.todotarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodotarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
